76ce3d22c5b641f0ce7314c8a3c85d6d00ad9510
devindavis5 <devinrdavis555@gmail.com>
2020-10-14 17:10:10 -0500

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function testFunc() {
 }
 
 const superbowlWin = (array) => {
-  array.find( function(result) { return result === "W" })
+  console.log(array)
+  // array.find( function(result) { return result === "W" })
+  let result = array.find( object => object.result === "W")
+   return !!result ? result.year :undefined
 }
